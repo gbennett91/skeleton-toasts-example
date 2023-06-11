33538306801abd4412edb95dffa02bef336e7269
Gareth <garethbennett91@gmail.com>
2023-06-11 01:33:05 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { toastStore } from '@skeletonlabs/skeleton';
+
+	function showToast() {
+		toastStore.trigger({ message: 'Hello' });
+	}
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<button class="btn variant-filled" on:click={showToast}>Click Me!</button>
+</div>
